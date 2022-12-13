209b51819de1eae007d0f27e0ecfc3751eeca1b2
Leonardo Torres <leonardo.torres@vallegrande.edu.pe>
2022-12-13 13:37:09 -0500

    Landing

---
+++ landing page/connect.js
@@ -0,0 +1,49 @@
+var express = require("express");
+var mysql = require("mysql");
+var app = express();
+var cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static(__dirname + '/'));
+
+var conexion = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "admin",
+  database: "db_landing_page"
+});
+
+conexion.connect(function (error) {
+  if (error) {
+    console.log(error)
+    throw error;
+  } else {
+    console.log("Conexión exitosa");
+  }
+});
+
+const puerto = process.env.PUERTO || 3000;
+
+app.listen(puerto, function () {
+  console.log("Servidor funcionando en puerto: " + puerto);
+});
+
+app.post("/api/contactanos", (req, res) => {
+	console.log('datos : ', req.body);
+	let data = {
+    	nomcon: req.body.nombre,
+    	corrcon: req.body.correo,
+    	asucon: req.body.asunto,
+    	descon: req.body.descripcion
+	};
+	let sql = "INSERT INTO contactanos SET ?";
+	conexion.query(sql, data, function (error, results) {
+  	if (error) {
+    	throw error;
+  	} else {
+    	console.log(data);
+    	res.send(data);
+  	}
+	});
+  });
---
+++ landing page/js/ventana.js
@@ -0,0 +1,7 @@
+function abrirVentana() {
+    document.getElementById("ventana-id").style.display = "flex";
+}
+
+function cerrarVentana() {
+    document.getElementById("ventana-id").style.display = "none";
+}
